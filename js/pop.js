d5f4cb77a74d21c30a3d8d299f1527f584c86d03
Aitor Mentxaka <aitormentxakaolivera@gmail.com>
2022-11-09 16:08:46 +0100

    change in pop function

--- js/pop.js
+++ js/pop.js
@@ -1,8 +1,7 @@
-function pop(y) {
+function pop(array) {
     x = [];
-    for (let i = 0; i < length(y) - 1; i++) {
-        x[i] = y[i];
+    for (let i = 0; i < length(array) - 1; i++) {
+        x[i] = array[i];
     }
-    x[length(x) - 1];
-    return y[length(y) - 1];
+    return array[length(array) - 1];
 }
